e77f12f89b3520ef863aa379edb4be4de583123a
NiccoF <111521406+NiccoF@users.noreply.github.com>
2024-04-15 11:59:35 -0500

    Added State Change Detection

--- JackAndBeanstalkCode.ino
+++ JackAndBeanstalkCode.ino
@@ -6,8 +6,12 @@ const int buttonPin = 2;
 bool canFeFiFoFum; // Boolean that assures players cannot activate the giant stomping early
 bool isFlipped;
 
+// state change
+int buttonState = 0;
+int prevButtonState = 0;
+
 void setup() {
-  myservo.attach(9);
+  myservo.attach(9); // Pin that servo is attached to
   pinMode(buttonPin, INPUT);
 
   isFlipped = false;
@@ -15,15 +19,28 @@ void setup() {
 }
 
 void loop() {
-  ActivateServo(myservo, buttonPin, 180);
+  //ActivateServo(myservo, buttonPin, 180);
+
+  // State change detection
+  buttonState = digitalRead(buttonPin);
+
+  if (buttonState != prevButtonState) {
+    if (buttonState == HIGH) {
+      ActivateServo(myservo, buttonPin, 180);
+    } else {
+      //chill ig
+    }
+  }
+  prevButtonState = buttonState;
 }
 
 // This method allows for any servo to be moved, passing any differences in the servos as parameters.
 void ActivateServo(Servo servo, int inputPin, int angle) {
-  if (digitalRead(inputPin) == HIGH && !isFlipped) {
-    servo.write(angle)
+  if (!isFlipped) {
+    servo.write(angle);
     isFlipped = true;
-  } else {
+  } else if(isFlipped) {
     servo.write(0);
+    isFlipped = false;
   }
 }
